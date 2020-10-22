c7c4be10be65cd858109db17f608be78fe374332
efique <valentin.barriol@ynov.com>
2020-10-22 15:24:34 +0200

    post

---
+++ migrations/Version20201022084830.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201022084830 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE question_answer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE question_historic_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE question_answer (id INT NOT NULL, title VARCHAR(255) NOT NULL, promoted BOOLEAN NOT NULL, status VARCHAR(255) NOT NULL, answers TEXT NOT NULL, created_at DATE NOT NULL, updated_at DATE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN question_answer.answers IS \'(DC2Type:json)\'');
+        $this->addSql('CREATE TABLE question_historic (id INT NOT NULL, title VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, updated_at DATE NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE question_answer_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE question_historic_id_seq CASCADE');
+        $this->addSql('DROP TABLE question_answer');
+        $this->addSql('DROP TABLE question_historic');
+    }
+}
--- src/Controller/QAController.php
+++ src/Controller/QAController.php
@@ -5,22 +5,52 @@ namespace App\Controller;
 use App\Entity\QuestionAnswer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class QAController extends AbstractController
 {
+    const STATUS_FAQ = 'faq';
+    const STATUS_BOT = 'bot';
+
     /**
      * @Route("/qa", name="create_qa")
      */
-    public function createQA(): Response
+    public function createQA(Request $request): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
 
+        $date = new \DateTime('now');
+        settype($body, "string");
+        settype($channel, "string");
+        $body = "test";
+        $channel = self::STATUS_FAQ;
+        
+        // echo $request->request->all();
+        // echo $request->get("title");
+        // echo $request->getContent();
+
         $qa = new QuestionAnswer();
-        $qa->setTitle();
-        $qa->setPromoted();
-        $qa->setStatus();
-        $qa->setAnswers();
-        $qa->setCreatedAt();
+        // $qa->setTitle($request->get("title"));
+        // $qa->setPromoted($request->get('promoted'));
+        // $qa->setStatus($request->get('status'));
+        // if (!in_array($channel, array(self::STATUS_FAQ, self::STATUS_BOT))) {
+        //     throw new \InvalidArgumentException("Invalid status");
+        // }else{
+        //     // $qa->setAnswers([$channel,$body]);
+        //     $qa->setAnswers($request->get('answers'));
+        // }
+        // $qa->setCreatedAt($date);
+
+        $qa->setTitle("test");
+        $qa->setPromoted(false);
+        $qa->setStatus("projet");
+        if (!in_array($channel, array(self::STATUS_FAQ, self::STATUS_BOT))) {
+            throw new \InvalidArgumentException("Invalid status");
+        }else{
+            $qa->setAnswers([$channel,$body]);
+        }
+        $qa->setCreatedAt($date);
 
         $entityManager->persist($qa);
 
